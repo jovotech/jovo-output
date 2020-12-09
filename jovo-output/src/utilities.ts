d349d4dd3ff049ad83a990db5d68a112352372e9
Max Ripper <max@jovo.tech>
2020-12-09 12:56:52 +0100

    :bug: Add missing optional validation-rule for platform-specific output

--- jovo-output/src/utilities.ts
+++ jovo-output/src/utilities.ts
@@ -1,11 +1,12 @@
 import { Type } from 'class-transformer';
-import { GenericOutput, ValidateNested } from '.';
+import { GenericOutput, IsOptional, ValidateNested } from '.';
 
 // TODO: find a better name
 export function decoratePropertyOfGenericOutput<TYPE extends Record<string, unknown>>(
   propertyKey: string,
   propertyType: new () => TYPE,
 ): void {
+  IsOptional()(GenericOutput.prototype, propertyKey);
   ValidateNested()(GenericOutput.prototype, propertyKey);
   Type(() => propertyType)(GenericOutput.prototype, propertyKey);
 }
