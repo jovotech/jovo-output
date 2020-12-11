35167c0d7e36c55f3430d9e0ae34222939125139
Max Ripper <max@jovo.tech>
2020-12-11 16:20:41 +0100

    :truck: Move web-components to dedicated folder

--- web-packages/jovo-output-components-vue/src/index.ts
+++ web-packages/jovo-output-components-vue/src/index.ts
@@ -7,7 +7,7 @@ import './assets/css/theme.css';
 
 declare global {
   interface Window {
-    JovoOutputComponentsVue: typeof import('.');
+    JovoOutputComponentsVue: typeof import('./index');
   }
 }
 
