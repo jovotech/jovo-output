a290ca7a5f5d0472f43388e0ad6bc7ebf33dbde2
Max Ripper <max@jovo.tech>
2021-01-15 14:23:36 +0100

    :sparkles: :construction:  Make it possible to directly edit generic-displays

--- web-packages/jovo-output-components-vue/src/components/output/GenericCarouselDisplay.vue
+++ web-packages/jovo-output-components-vue/src/components/output/GenericCarouselDisplay.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="border p-2 overflow-x-auto">
-    <h2 v-if="carousel.title" class="text-lg font-semibold mb-2">{{ carousel.title }}</h2>
+    <h2 v-if="carousel.title || isEditable" class="text-lg font-semibold mb-2">
+      <auto-resize-input
+        v-if="isEditable"
+        v-model="carousel.title"
+        placeholder="Title"
+        @input="handleTitleInput"
+      />
+      <span v-else>{{ carousel.title }}</span>
+    </h2>
     <div
       ref="cardContainer"
       class="flex overflow-x-auto pb-2 space-x-3"
@@ -9,8 +17,10 @@
       <generic-card-display
         v-for="(item, index) in carousel.items"
         :key="index"
+        :is-editable="isEditable"
         class="flex-shrink-0 bg-white"
         :card="item"
+        @input="handleCardInput($event, index)"
       />
     </div>
   </div>
@@ -18,15 +28,43 @@
 
 <script lang="ts">
 import GenericCardDisplay from '@/components/output/GenericCardDisplay.vue';
-import { GenericCarousel } from 'jovo-output';
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import AutoResizeInput from '@/components/ui/AutoResizeInput.vue';
+import { GenericCard, GenericCarousel } from 'jovo-output';
+import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
 
 @Component({
   name: 'generic-carousel-display',
-  components: { GenericCardDisplay },
+  components: { AutoResizeInput, GenericCardDisplay },
 })
 export default class GenericCarouselDisplay extends Vue {
   @Prop({ required: true, type: Object })
   carousel!: GenericCarousel;
+
+  @Prop({ required: false, type: Boolean, default: false })
+  isEditable!: boolean;
+
+  editObject: GenericCarousel = { title: '', items: [] };
+
+  handleTitleInput(title: string) {
+    this.handleInput({ ...this.carousel, title });
+  }
+
+  handleCardInput(card: GenericCard, index: number) {
+    const items = this.carousel.items.slice();
+    items[index] = card;
+    this.handleInput({
+      ...this.carousel,
+      items,
+    });
+  }
+
+  handleInput(newCarousel: GenericCarousel) {
+    this.$emit('input', newCarousel);
+  }
+
+  @Watch('carousel', { deep: true, immediate: true })
+  onCarouselChange() {
+    this.editObject = { ...this.carousel };
+  }
 }
 </script>
--- web-packages/jovo-output-components-vue/src/components/output/GenericQuickReplyDisplay.vue
+++ web-packages/jovo-output-components-vue/src/components/output/GenericQuickReplyDisplay.vue
@@ -1,22 +1,53 @@
 <template>
   <div class="inline-flex border border-black bg-white px-4 py-1 rounded-full cursor-pointer">
-    {{ text }}
+    <auto-resize-input
+      v-if="isEditable"
+      v-model="editText"
+      @input="handleInput"
+      placeholder="Quick-reply"
+    />
+    <div class="block" v-else>
+      {{ text }}
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import AutoResizeInput from '@/components/ui/AutoResizeInput.vue';
 import { QuickReply } from 'jovo-output';
+import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
 
 @Component({
   name: 'generic-quick-reply-display',
+  components: { AutoResizeInput },
 })
 export default class GenericQuickReplyDisplay extends Vue {
   @Prop({ required: true, type: [Object, String] })
   quickReply!: QuickReply;
 
+  @Prop({ required: false, type: Boolean, default: false })
+  isEditable!: boolean;
+
+  editText = '';
+
   get text(): string {
     return typeof this.quickReply === 'string' ? this.quickReply : this.quickReply.text;
+  }
+
+  handleInput() {
+    const newQuickReply: QuickReply =
+      typeof this.quickReply === 'string'
+        ? this.editText
+        : {
+            text: this.editText,
+            value: this.quickReply.value,
+          };
+    this.$emit('input', newQuickReply);
+  }
+
+  @Watch('quickReply', { deep: true, immediate: true })
+  onQuickReplyChange() {
+    this.editText = this.text;
   }
 }
 </script>
