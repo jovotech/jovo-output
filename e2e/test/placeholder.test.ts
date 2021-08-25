87f3bd6cf82d5de8e97abbe0a560994b49c473ea
Max Ripper <max@jovo.tech>
2021-08-25 13:40:06 +0200

    :white_check_mark: Adjust existing tests

--- e2e/test/placeholder.test.ts
+++ e2e/test/placeholder.test.ts
@@ -1,15 +1,3 @@
-import { OutputTemplate, OutputTemplateConverter } from '@jovotech/output';
-import { AlexaOutputTemplateConverterStrategy } from '@jovotech/output-alexa';
-
-const outputConverter = new OutputTemplateConverter(new AlexaOutputTemplateConverterStrategy());
-
-const output: OutputTemplate = {
-  message: "What's your name?",
-  reprompt: "What's your name/",
-
-  Alexa: {},
-};
-
 test('placeholder', async () => {
   expect(true).toBe(true);
 });
